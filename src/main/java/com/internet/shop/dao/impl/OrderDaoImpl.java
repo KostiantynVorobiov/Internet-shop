acf46264c1d3340f9cda68724d486a0a6c30789f
Kostian <pyteec.ua@i.ua>
2020-09-07 14:55:55 +0300

    added Dao and DaoImpl for all models, and added list models to storage

---
+++ src/main/java/com/internet/shop/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,41 @@
+package com.internet.shop.dao.impl;
+
+import com.internet.shop.dao.OrderDao;
+import com.internet.shop.db.Storage;
+import com.internet.shop.model.Order;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+public class OrderDaoImpl implements OrderDao {
+    @Override
+    public Order create(Order order) {
+        Storage.addOrder(order);
+        return order;
+    }
+
+    @Override
+    public Optional<Order> getById(Long id) {
+        return Storage.orders.stream()
+                .filter(order -> order.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public List<Order> getAll() {
+        return Storage.orders;
+    }
+
+    @Override
+    public Order update(Order order) {
+        IntStream.range(0, Storage.orders.size())
+                .filter(index -> Storage.orders.get(index).equals(order.getId()))
+                .forEach(index -> Storage.orders.set(index, order));
+        return order;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return Storage.orders.removeIf(order -> order.getId().equals(id));
+    }
+}
