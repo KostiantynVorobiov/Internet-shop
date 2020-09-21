2774714a7ba5013fc0cb482cd302a1cb885e5f40
Kostian <pyteec.ua@i.ua>
2020-09-21 17:04:23 +0300

    changed method parameters to (Product product), made new method getProductFromResultSet, deleted unique from name in product table

--- src/main/java/com/internet/shop/dao/jdbc/ProductDaoJdbcImpl.java
+++ src/main/java/com/internet/shop/dao/jdbc/ProductDaoJdbcImpl.java
@@ -18,22 +18,21 @@ import java.util.Optional;
 public class ProductDaoJdbcImpl implements ProductDao {
 
     @Override
-    public Product create(Product item) {
+    public Product create(Product product) {
         String query = "INSERT INTO products (name, price) VALUES (?, ?)";
         try (Connection connection = ConnectionUtil.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(query,
                     Statement.RETURN_GENERATED_KEYS);
-            preparedStatement.setString(1, item.getName());
-            preparedStatement.setDouble(2, item.getPrice());
+            preparedStatement.setString(1, product.getName());
+            preparedStatement.setDouble(2, product.getPrice());
             preparedStatement.execute();
             ResultSet resultSet = preparedStatement.getGeneratedKeys();
             while (resultSet.next()) {
-                Long productId = resultSet.getLong(1);
-                item.setId(productId);
+                product.setId(resultSet.getLong(1));
             }
-            return item;
+            return product;
         } catch (SQLException e) {
-            throw new DataOperationException("Can't create product: " + item.getName(), e);
+            throw new DataOperationException("Can't create product: " + product.getName(), e);
         }
     }
 
@@ -45,11 +44,7 @@ public class ProductDaoJdbcImpl implements ProductDao {
             preparedStatement.setLong(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
-                Long productId = resultSet.getLong("product_id");
-                String name = resultSet.getString("name");
-                Double price = resultSet.getDouble("price");
-                Product product = new Product(name, price);
-                product.setId(productId);
+                Product product = getProductFromResultSet(resultSet);
                 return Optional.of(product);
             }
         } catch (SQLException e) {
@@ -66,12 +61,7 @@ public class ProductDaoJdbcImpl implements ProductDao {
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
-                long productId = resultSet.getLong("product_id");
-                String name = resultSet.getString("name");
-                Double price = resultSet.getDouble("price");
-                Product product = new Product(name, price);
-                product.setId(productId);
-                productList.add(product);
+                productList.add(getProductFromResultSet(resultSet));
             }
             return productList;
         } catch (SQLException e) {
@@ -80,18 +70,18 @@ public class ProductDaoJdbcImpl implements ProductDao {
     }
 
     @Override
-    public Product update(Product item) {
+    public Product update(Product product) {
         String query = "UPDATE products SET name=?, price=? "
                 + "WHERE deleted = false AND product_id = ?";
         try (Connection connection = ConnectionUtil.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, item.getName());
-            preparedStatement.setDouble(2, item.getPrice());
-            preparedStatement.setLong(3, item.getId());
+            preparedStatement.setString(1, product.getName());
+            preparedStatement.setDouble(2, product.getPrice());
+            preparedStatement.setLong(3, product.getId());
             preparedStatement.executeUpdate();
-            return item;
+            return product;
         } catch (SQLException e) {
-            throw new DataOperationException("Can't update product: " + item.getName(), e);
+            throw new DataOperationException("Can't update product: " + product.getName(), e);
         }
     }
 
@@ -105,5 +95,14 @@ public class ProductDaoJdbcImpl implements ProductDao {
         } catch (SQLException e) {
             throw new DataOperationException("Can't deleted product with id: " + id, e);
         }
+    }
+
+    private Product getProductFromResultSet(ResultSet resultSet) throws SQLException {
+        long productId = resultSet.getLong("product_id");
+        String name = resultSet.getString("name");
+        double price = resultSet.getDouble("price");
+        Product product = new Product(name, price);
+        product.setId(productId);
+        return product;
     }
 }
